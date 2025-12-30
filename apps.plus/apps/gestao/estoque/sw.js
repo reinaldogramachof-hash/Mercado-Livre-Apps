62914e050732d2df9ab4b1465c3722e31acc119c
reinaldogramachof-hash <reinaldogramachof@gmail.com>
2025-12-30 14:00:56 -0300

    Adiciona estrutura inicial PWA para os novos aplicativos PDV, Agenda, Estoque e OS Express.

---
+++ apps.plus/apps/gestao/estoque/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'estoque-plena-v1';
+const ASSETS = [
+    './index.html',
+    './plus.html',
+    './manifest.json',
+    'https://cdn.tailwindcss.com',
+    'https://unpkg.com/lucide@latest'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
